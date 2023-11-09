4ba524c5a3a213421676337fbf8885a2e4ab19cc
Benex Drake <benex@hotmail.de>
2023-11-09 11:20:20 +0100

    some changes made

--- src/embeds/animeEmbed.ts
+++ src/embeds/animeEmbed.ts
@@ -5,7 +5,6 @@ export class AnimeEmbed
 {
     Build(anime:Anime) : EmbedBuilder
     {
-        
         const embed = new EmbedBuilder();
 
         embed.setTitle(anime.title)
@@ -13,13 +12,16 @@ export class AnimeEmbed
              .setColor(Colors.Orange)
              .setURL(anime.url)
              .setDescription(anime.description || ' ')
-             .setImage(anime.imageUrl);
+             .setImage(anime.imageUrl_banner);
              
         if(anime.publisher !== '')
             embed.addFields({name: 'Publisher', value: anime.publisher})
-        if(anime.language !== '')
-            embed.addFields({name: 'Language', value: anime.language})
-        if(anime.tags.length > 0)
+        if(anime.audios.length > 0)
+            embed.addFields({name: 'Language', value: anime.audios.join(', ')})
+            if(anime.subTitles.length > 0)
+            embed.addFields({name: 'Language', value: anime.subTitles.join(', ')})
+
+        if(anime.tags?.length > 0 && anime.tags)
         {
             if(!anime.tags.includes(''))
                 embed.addFields({name: 'Tags', value: anime.tags.join(', ')})
@@ -41,4 +43,22 @@ export class AnimeEmbed
 
         return embed;
     }
+}
+
+function base64Decode(input:string) {
+    // Replace non-url compatible chars with base64 standard chars
+    input = input
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    // Pad out with standard base64 required padding characters
+    var pad = input.length % 4;
+    if(pad) {
+      if(pad === 1) {
+        throw new Error('InvalidLengthError: Input base64url string is the wrong length to determine padding');
+      }
+      input += new Array(5-pad).join('=');
+    }
+
+    return input;
 }
